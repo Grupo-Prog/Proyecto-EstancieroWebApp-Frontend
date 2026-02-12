e1285cfbc74268432bf64515792874d1d527906d
412285-Cotaimich <412285@tecnicatura.frc.utn.edu.ar>
2026-02-12 02:16:41 -0300

    crear usuario desde Angular

--- frontend/src/app/pages/create-user/create-user.ts
+++ frontend/src/app/pages/create-user/create-user.ts
@@ -1,19 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {Router} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {UserService} from '../../core/services/user/user-service';
 
 @Component({
   selector: 'app-create-user',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './create-user.html',
   styleUrl: './create-user.css',
   standalone: true
 })
 export class CreateUser {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+              private userService: UserService,) {}
+
+  email = '';
+  password = '';
+  name ='';
 
   goToLoginPage() {
     this.router.navigate(['/login']);
+  }
+
+  create(): void {
+    const dto = {
+      email: this.email,
+      password: this.password,
+      name: this.name
+    };
+
+    this.userService.create(dto).subscribe({
+      next: (user) => {
+        console.log("Useer created", user);
+        this.router.navigate(['login']);
+      },
+      error: () => alert('Creation error!')
+    });
   }
 
 }
