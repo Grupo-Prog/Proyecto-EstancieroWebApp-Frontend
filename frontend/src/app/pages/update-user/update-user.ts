418a7b9838b3f2aaf7d286f0477845e539b563b2
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-02-19 01:29:21 -0300

    feat: add update user info form
    necesita mejorar
    
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

--- frontend/src/app/pages/update-user/update-user.ts
+++ frontend/src/app/pages/update-user/update-user.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { BackButtonComponent } from '../../core/components/back-button-component/back-button-component';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserService } from '../../core/services/user/user-service';
 
 @Component({
   selector: 'app-update-user',
-  imports: [],
+  imports: [BackButtonComponent, ReactiveFormsModule],
   templateUrl: './update-user.html',
   styleUrl: './update-user.css',
-  standalone: true
+  standalone: true,
 })
-export class UpdateUser {
+export class UpdateUser implements OnInit {
+  private fb = inject(FormBuilder);
+  private userService = inject(UserService);
+  userForm = this.fb.group({
+    id: [0],
+    username: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+  });
 
+  ngOnInit(): void {
+    this.cargarDatosUsuario();
+  }
+  cargarDatosUsuario() {
+    //datos de prueba
+    const user = {
+      id: 123,
+      username: 'John Doe',
+      email: '5oXtP@example.com',
+    };
+
+    // this.userService.findById(user.id).subscribe((user) => {
+    //    this.userForm.patchValue(user);
+    //  });
+
+    this.userForm.patchValue(user);
+  }
+  onSubmit() {
+    if (this.userForm.valid) {
+      const datosParaEnviar = this.userForm.value;
+      console.log('Enviando update:', datosParaEnviar);
+
+      // this.userService.update(datosParaEnviar)...
+    } else {
+      this.userForm.markAllAsTouched();
+    }
+  }
 }
