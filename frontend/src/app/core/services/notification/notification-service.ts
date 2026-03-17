09f8e2045aeef18a2e1798c6cb2a83ca51aef899
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-03-17 02:21:21 -0300

    refactor: notificationService uses snackbar

--- frontend/src/app/core/services/notification/notification-service.ts
+++ frontend/src/app/core/services/notification/notification-service.ts
@@ -1,14 +1,38 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NotificationService {
-  private errorSubject = new Subject<string>();
-  public error$ = this.errorSubject.asObservable();
+  private snackBar = inject(MatSnackBar);
 
-  showError(arg0: string) {
-    throw new Error('Method not implemented.');
+  private baseConfig: MatSnackBarConfig = {
+    duration: 4000,
+    horizontalPosition: 'right',
+    verticalPosition: 'bottom',
+  };
+
+  showError(message: string) {
+    this.snackBar.open(message, 'Cerrar', {
+      ...this.baseConfig,
+      panelClass: ['snack-error'],
+    });
+  }
+
+  showWarning(message: string) {
+    this.snackBar.open(message, 'Cerrar', {
+      ...this.baseConfig,
+      panelClass: ['snack-warning'],
+    });
+  }
+
+  showSuccess(message: string): void {
+    this.snackBar.open(message, 'OK', {
+      ...this.baseConfig,
+      duration: 2500,
+      panelClass: ['snack-success'],
+    });
   }
 }
